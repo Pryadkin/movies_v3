edef418d6657088188e5209bed12a60d8963413e
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2025-02-24 21:09:54 +0400

    update eslintConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,45 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {FlatCompat} from '@eslint/eslintrc'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+    // Подключаем конфигурации Next.js
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-export default eslintConfig;
+    // Дополнительные правила для TypeScript
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn', // Предупреждение при неиспользуемых переменных
+            '@typescript-eslint/no-explicit-any': 'warn', // Предупреждение при использовании `any`
+        },
+    },
+
+    // Дополнительные правила для React
+    {
+        files: ['**/*.jsx', '**/*.tsx'],
+        rules: {
+            'react/jsx-uses-react': 'error', // Обнаружение использования React
+            'react/jsx-uses-vars': 'error', // Обнаружение неиспользуемых переменных в JSX
+            'react/react-in-jsx-scope': 'off', // Не требуется в React 17+
+        },
+    },
+
+    // Общие правила для всех файлов
+    {
+        rules: {
+            'no-console': 'warn', // Предупреждение при использовании console.log
+            'no-unused-vars': 'warn', // Предупреждение при неиспользуемых переменных
+            'no-undef': 'error', // Ошибка при использовании необъявленных переменных
+        },
+    },
+]
+
+export default eslintConfig
--- installGuid.md
+++ installGuid.md
@@ -1,4 +1,5 @@
 1. npx create-next-app@latest
-2. yarn add prettier eslint-config-prettier -D
+2. yarn add prettier -D eslint-config-prettier @trivago/prettier-plugin-sort-imports prettier-plugin-tailwindcss
 3. add .prettier.json
 4. eslint поддерживает новый формат конфига - Flat Config. (eslint.config.mjs)
+    Добавляем туда нужные правила
